597a01ead558b66e5b262d1ad3f27bd1a4152a7b
Enzo Evers <enzoevers@gmail.com>
2023-12-05 16:35:21 +0100

    test: created testSchematicReader_getBlock_noFileReaderMock

---
+++ day3/test/testSchematicReader_getBlock_noFileReaderMock.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "../include/FileReader.hpp"
+#include "../include/SchematicReader.hpp"
+#include "gtest/gtest.h"
+#include <filesystem>
+
+using ::testing::_;
+
+TEST(testSchematicReader_getBlock_noFileReaderMock,
+     getSquareBlockStartingAtZeroZero) {
+    MatrixCoordinate topLeftCoordinate(0, 0);
+
+    std::pair<std::size_t, std::size_t> blockSize(3, 3);
+    std::vector<char> block;
+    block.reserve(blockSize.first * blockSize.second);
+
+    std::string absolutePath = (std::filesystem::current_path() /
+                                "./test/testSchematics/schematic1.txt")
+                                   .string();
+
+    FileReader fileReader(absolutePath);
+    SchematicReader reader(fileReader);
+
+    bool ret = reader.getBlock(topLeftCoordinate, block, blockSize);
+
+    ASSERT_TRUE(ret);
+    ASSERT_EQ(block.size(), blockSize.first * blockSize.second);
+    ASSERT_THAT(block, testing::ElementsAre('.', '.', '.', '.', '.', '2', '.',
+                                            '.', '.'));
+}
+
+TEST(testSchematicReader_getBlock_noFileReaderMock,
+     getNoneSquareBlockStartingAtZeroZero) {
+    MatrixCoordinate topLeftCoordinate(0, 0);
+
+    std::pair<std::size_t, std::size_t> blockSize(4, 3);
+    std::vector<char> block;
+    block.reserve(blockSize.first * blockSize.second);
+
+    std::string absolutePath = (std::filesystem::current_path() /
+                                "./test/testSchematics/schematic1.txt")
+                                   .string();
+
+    FileReader fileReader(absolutePath);
+    SchematicReader reader(fileReader);
+
+    bool ret = reader.getBlock(topLeftCoordinate, block, blockSize);
+
+    ASSERT_TRUE(ret);
+    ASSERT_EQ(block.size(), blockSize.first * blockSize.second);
+    ASSERT_THAT(block, testing::ElementsAre('.', '.', '.', '#', '.', '.', '2',
+                                            '.', '.', '.', '.', '.'));
+}
+
+TEST(testSchematicReader_getBlock_noFileReaderMock,
+     getSquareBlockStartingAtNonZeroZeroInsideFile) {
+    MatrixCoordinate topLeftCoordinate(3, 2);
+
+    std::pair<std::size_t, std::size_t> blockSize(3, 3);
+    std::vector<char> block;
+    block.reserve(blockSize.first * blockSize.second);
+
+    std::string absolutePath = (std::filesystem::current_path() /
+                                "./test/testSchematics/schematic1.txt")
+                                   .string();
+
+    FileReader fileReader(absolutePath);
+    SchematicReader reader(fileReader);
+
+    bool ret = reader.getBlock(topLeftCoordinate, block, blockSize);
+
+    ASSERT_TRUE(ret);
+    ASSERT_EQ(block.size(), blockSize.first * blockSize.second);
+    ASSERT_THAT(block, testing::ElementsAre('.', '.', '$', '.', '.', '3', '.',
+                                            '.', '2'));
+}
